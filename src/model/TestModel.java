26a694066119c291787eb1627100463cfca66636
samuele <s.caporale1@campus.uniurb.it>
2025-03-29 17:50:13 +0100

    secondo player test commit

--- src/model/TestModel.java
+++ src/model/TestModel.java
@@ -5,8 +5,10 @@ public class TestModel {
 	public static void main(String[] args) {
 	
 		Player p1 = new Player("Samuele",1);
+		Player p2 = new Player("Cristino",1);
 		
 		System.out.print(p1.toString());
+		System.out.print(p2.toString());
 
 	}
 
